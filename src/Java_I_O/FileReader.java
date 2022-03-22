4dafeb7950f73fd3d6c14eadc177f0f6af2dffbc
rahul <rahul.kumar_cs19@gla.ac.in>
2022-03-23 00:49:37 +0530

    close fileInputStream

--- src/Java_I_O/FileReader.java
+++ src/Java_I_O/FileReader.java
@@ -2,6 +2,7 @@ package Java_I_O;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileReader {
     public static void main(String[] args) {
@@ -14,5 +15,26 @@ public class FileReader {
             System.err.println("Could not open file, please check path and file name");
 
         }
+        //now the file is open to be read
+        try {
+            do {
+                readCharacter = fileInputStream.read();
+                if (readCharacter != -1) {
+                    System.out.println((char) readCharacter);
+                }
+            }
+            while (readCharacter != -1);
+        } catch (IOException ioException) {
+            System.err.println("Error in reading file");
+        } catch (NullPointerException nullPointerException) {
+            System.err.println("The file input stream reference was null");
+        } finally {
+            try {
+                fileInputStream.close();
+            } catch (IOException ioException) {
+                System.err.println("Error in closing the file");
+            }
+        }
+
     }
 }
