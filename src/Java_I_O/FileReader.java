cd916004f7e0e126d7a452aef4130e984277206f
rahul <rahul.kumar_cs19@gla.ac.in>
2022-03-23 00:43:18 +0530

    Create FileInputStream object

--- src/Java_I_O/FileReader.java
+++ src/Java_I_O/FileReader.java
@@ -1,7 +1,18 @@
 package Java_I_O;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class FileReader {
     public static void main(String[] args) {
         int readCharacter;
+        FileInputStream fileInputStream = null;
+        try {
+
+            fileInputStream = new FileInputStream("Class.php");
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.err.println("Could not open file, please check path and file name");
+
+        }
     }
 }
